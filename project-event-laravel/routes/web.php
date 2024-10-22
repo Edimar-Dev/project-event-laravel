326a413ae5c5d031f2631e72ef934c6cc78aecb4
Edimar Sanches dos Prazeres Nunes <edimarsanchesnunes@gmail.com>
2024-10-22 16:02:27 -0300

    passos inicias - testando  as funcionalidades do laravel blade views routes

--- project-event-laravel/routes/web.php
+++ project-event-laravel/routes/web.php
@@ -3,5 +3,20 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $nome = 'Edimar';
+    $idade = 30;
+    $profissao = 'N1';
+    
+    return view('welcome', ['nome' => $nome, 'idade' => $idade, 'profissao' => $profissao]);
+});
+
+// create view (criação da viu da página de contato) contato.blade.php
+Route::get('/contato', function () {
+    return view('contato');
+});
+
+// create view (criação da viu da página de produto) produto.blade.php
+Route::get('/produto', function () {
+    return view('produto');
 });
