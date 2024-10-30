38a065d48384c70baae80937c09f1dda81a03a4e
Edimar Sanches dos Prazeres Nunes <edimarsanchesnunes@gmail.com>
2024-10-30 11:22:21 -0300

    testes das migrates inserindo e excluíndos tabelas - migrate/rollback

--- project-event-laravel/database/migrations/2024_10_30_134043_add_category_to_produtos_table.php
+++ project-event-laravel/database/migrations/2024_10_30_134043_add_category_to_produtos_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
     {
         Schema::table('produtos', function (Blueprint $table) {
             //
-            $table->string('categoria', 100);
+            $table->dropColumn('categoria');
         });
     }
 };
