4ea205fb3dff60749bd68eccc67200dfac37057b
Edimar Sanches dos Prazeres Nunes <edimarsanchesnunes@gmail.com>
2024-11-21 16:10:30 -0300

    adiciona estilos e estrutura para a criação de eventos na página

--- project-event-laravel/resources/views/events/create.blade.php
+++ project-event-laravel/resources/views/events/create.blade.php
@@ -4,29 +4,27 @@
 
 @section('content')
 
-<h1>Criar um Evento</h1>
-
 <div id="event-create-container" class="col-md-6 offset-md-3">
     <h1>Crie seu evento</h1>
     <form action="/events" method="POST">
         @csrf
-        <div class="form-group">
-            <label for="title">Eventos:</label>
+        <div class="form-group pb-3">
+            <label for="title" class="pb-3">Evento:</label>
             <input type="text" class="form-control" id="title" name="title" placeholder="Nome do Evento">
         </div>
-        <div class="form-group">
-            <label for="title">Cidade:</label>
+        <div class="form-group pb-3">
+            <label for="title" class="pb-3">Cidade:</label>
             <input type="text" class="form-control" id="city" name="city" placeholder="Local do Evento">
         </div>
-        <div class="form-group">
-            <label for="title">O evento é privado?:</label>
+        <div class="form-group pb-3">
+            <label for="title" class="pb-3">O evento é privado?:</label>
             <select name="private" id="private" class="form-control">
                 <option value="0">Sim</option>
                 <option value="1">Não</option>
             </select>
         </div>
-        <div class="form-group">
-            <label for="title">Descrição:</label>
+        <div class="form-group pb-3">
+            <label for="title" class="pb-3">Descrição:</label>
             <textarea name="description" id="description" class="form-control" placeholder="Descreva o que vai acontecer no evento"></textarea>
         </div>
         <input type="submit" class="btn btn-primary" value="Criar Evento">
