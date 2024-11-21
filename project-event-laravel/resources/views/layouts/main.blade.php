596f1b1dc5aa2c5c917a3733af0e75bff7ff41ef
Edimar Sanches dos Prazeres Nunes <edimarsanchesnunes@gmail.com>
2024-11-21 10:43:41 -0300

    corrige caminhos de arquivos CSS e JS e remove padding do corpo

--- project-event-laravel/resources/views/layouts/main.blade.php
+++ project-event-laravel/resources/views/layouts/main.blade.php
@@ -11,23 +11,23 @@
         <!-- CSS Bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <!-- CSS da aplicação-->
-        <link rel="stylesheet" href="./css/styles.css">
+        <link rel="stylesheet" href="/css/styles.css">
         <!-- JS da aplicação -->
-        <script src="./js/script.js"></script> 
+        <script src="/js/script.js"></script> 
     </head>
     <body>
      <header>
         <nav class="navbar navbar-expand-lg navbar-light">
             <div class="collapse navbar-collapse" id="navbar">
                 <a href="/" class="navbar-brand">
-                    <img src="./img/logo1.png" alt="LOGO GERE UM COM">
+                    <img src="/img/logo1.png" alt="LOGO GERE UM COM">
                 </a>
                 <ul class="navbar-nav">
                     <li class="nav-intem">
                         <a href="/" class="nav-link">Eventos</a>
                     </li>
                     <li class="nav-intem">
-                        <a href="./events/create" class="nav-link">Criar Eventos</a>
+                        <a href="/events/create" class="nav-link">Criar Eventos</a>
                     </li>
                     <li class="nav-intem">
                         <a href="/events/create" class="nav-link">Entrar</a>
