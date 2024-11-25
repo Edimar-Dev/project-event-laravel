e12fcfcb4b88e1381cf3917d827de4fff9b6dac5
Edimar Sanches dos Prazeres Nunes <edimarsanchesnunes@gmail.com>
2024-11-25 11:41:53 -0300

    adiciona funcionalidade de upload de imagem ao criar evento

--- project-event-laravel/app/Http/Controllers/EventController.php
+++ project-event-laravel/app/Http/Controllers/EventController.php
@@ -29,6 +29,21 @@ class EventController extends Controller
         $event->private = $request->private;
         $event->description = $request->description;
 
+        // UPLOAD DE IMG
+        if($request->hasFile('imagem') && $request->file('imagem')->isValid()){
+
+            $requestImage = $request->image;
+
+            $extension = $requestImage->extension();
+
+            $imageName = md5($requestImage->image->getClientOriginalName() . strtotime('now') .".". $extension ); //gerar um nome único para a imagem
+
+            $requestImage->move(public_path('img/events'), $imageName); //mover a imagem para a pasta img/events
+
+            $event->image = $imageName;
+        }
+
+
         $event->save(); //salvar os dados no banco
 
         return redirect('/')->with('msg', 'Evento criado com sucesso!'); //redirecionar para página após salvar
